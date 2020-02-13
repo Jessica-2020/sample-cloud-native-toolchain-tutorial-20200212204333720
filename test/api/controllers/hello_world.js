27bfe7427ca1f95c8c63ba2821c8096320efbb3b
Jessica Sepúlveda <Jessica.Sepulveda@ibm.com>
2020-02-13 20:05:07 +0000

    Se modificó el Hello World con un Hello there

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -19,8 +19,8 @@ describe('controllers', function() {
           .expect(200)
           .end(function(err, res) {
             should.not.exist(err);
-
-            res.body.should.eql('Hello, stranger!');
+			
+            res.body.should.eql('Hello, there stranger!');
 
             done();
           });
@@ -39,7 +39,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello, Scott!');
+            res.body.should.eql('Hello, ther Scott!');
 
             done();
           });
